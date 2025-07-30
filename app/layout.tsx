d9d5b4baa4a6991b7c6215b2d497496126222246
Elua-b <bugingoeloi@gmail.com>
2025-07-30 09:26:22 +0200

    adding work

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Eloi dev',
   description: 'Created with v0',
-  generator: 'v0.dev',
+  generator: 'Eloi',
 }
 
 export default function RootLayout({
